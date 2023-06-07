12ac078f9ad69833a9b5312c983f07c8dc492132
Omid <o.shabani@hotmail.com>
2023-06-07 13:26:25 +0330

    implement computed functio for name

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <h2>{{ userName }}</h2>
     <h3>{{ user.age }}</h3>
     <button @click="setAge">Set Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
 // eslint-disable-next-line
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from "vue";
 export default {
   setup() {
     //use for single value
     // eslint-disable-next-line
-    let age = ref(31)
-    
+    let age = ref(31);
+    const firstName = ref("Omid");
+    const lastName = ref("Shabani");
+
     //use for object value
     let user = reactive({
-      name: 'OmidShabani',
-      age: 31
+      name: "OmidShabani",
+      age: 31,
     });
 
-    function setNewData(){
-      user.age = 33
+    const userName = computed(function () {
+      return firstName.value + " " + lastName.value;
+    });
+
+    function setNewData() {
+      user.age = 33;
+    }
+    function setFirstName(event) {
+      console.log(event.target.value);
+      firstName.value = event.target.value;
+    }
+    function setLastName(event) {
+      lastName.value = event.target.value;
     }
 
-    return { user: user, setAge: setNewData };
+    return {
+      user: user,
+      setAge: setNewData,
+      userName,
+      setFirstName,
+      setLastName,
+    };
   },
 };
 </script>
