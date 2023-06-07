3eaa5b9bea408e2412b8595ef2310f7f6feab068
Omid <o.shabani@hotmail.com>
2023-06-07 13:37:59 +0330

    use two-way binding to show name

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <h3>{{ user.age }}</h3>
     <button @click="setAge">Set Age</button>
     <div>
-      <input type="text" placeholder="First Name" @input="setFirstName" />
-      <input type="text" placeholder="Last Name" @input="setLastName" />
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
     </div>
   </section>
 </template>
@@ -34,20 +34,14 @@ export default {
     function setNewData() {
       user.age = 33;
     }
-    function setFirstName(event) {
-      console.log(event.target.value);
-      firstName.value = event.target.value;
-    }
-    function setLastName(event) {
-      lastName.value = event.target.value;
-    }
+    
 
     return {
       user: user,
       setAge: setNewData,
       userName,
-      setFirstName,
-      setLastName,
+      firstName,
+      lastName,
     };
   },
 };
